510a6bdee3015914ef36f4db28d1f4b0cc3da173
Ngô Bá Cường <ngobacuong2211@gmail.com>
2025-06-04 17:29:00 +0700

    Update doxaRoutes.js

--- noti-server/src/routes/doxaRoutes.js
+++ noti-server/src/routes/doxaRoutes.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { sendNotification } = require('../sockets');
+const { sendNotificationForDoxaDay } = require('../sockets');
 
 // Route riêng cho DOXADAY
 router.post('/', (req, res) => {
-    const { donationId } = req.body;
+    try {
 
-    if (!donationId || !donationId.includes("DOXADAY")) {
-        return res.status(400).json({ error: "Invalid or missing DOXADAY ID" });
+        const { donationId } = req.body;
+
+        if (!donationId || !donationId.includes("DOXADAY")) {
+            return res.status(400).json({ error: "Invalid or missing DOXADAY ID" });
+        }
+
+        console.log("📢 Trigger socket for DOXADAY:", donationId);
+        sendNotificationForDoxaDay(donationId); // 🔔 Gửi tới các client
+
+        return res.status(200).json({ success: true });
+    } catch (error) {
+        console.error('Error sending notification:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    console.log("📢 Trigger socket for DOXADAY:", donationId);
-    sendNotificationForDoxaDay(donationId); // 🔔 Gửi tới các client
-
-    return res.status(200).json({ success: true });
 });
 
 module.exports = router;
